3a1c5fe5a88c7bce09e03768861b80316513c891
hyunmin kim <oannes@hm.ucdenver.pvt>
2015-11-18 15:29:45 -0500

    seq.read

--- src/seq.sh
+++ src/seq.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 . $HMHOME/src/root.sh
 
+seq.read(){
+usage="
+USAGE: $FUNCNAME <fa> <bed> [options]
+ [options]: -s : reverse complements for the negative strand
+"
+local option=${3:-""};
+if [ $# -lt 2 ]; then echo "$usage"; return; fi
+	cat $1 | perl -e 'use strict; 
+		sub revc{
+			my ($seq) = @_;
+			$seq =~ tr/ACGTacgt/TGCAtgca/;
+			return join("",reverse(split //,$seq));
+		}
+
+		my $file="'$2'"; my $opt="'$option'";
+		my %ref=(); my $chrom="";
+		while(<STDIN>){ chomp;
+			if($_=~/>(\S+)/){ $chrom=$1; next; }		
+			$ref{$chrom} .= $_;
+		}
+		#foreach my $c (keys %ref){ print $c," ",substr($ref{$c},0,10),"\n"; }
+		open(my $fh, "<", $file) or die "$!";
+		while(<$fh>){chomp;
+			my @a=split/\t/,$_;
+			my $seq="NULL";
+			if(defined $ref{$a[0]}){
+				$seq=substr($ref{$a[0]},$a[1],$a[2]-$a[1]);
+				if($opt eq "-s"){
+					$seq=revc($seq);
+				}
+			}
+			print join("\t",@a),"\t",$seq,"\n";
+		}
+		close($fh);
+		
+	'
+}
+
 kmers(){
 ## obtained from Cook malcolm
 ## http://comments.gmane.org/gmane.comp.lang.perl.bio.general/18242
