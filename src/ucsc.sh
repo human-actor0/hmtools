27d1a91901d27f997d77f0c45777feddc8c5e534
hyunmin kim <oannes@hm.ucdenver.pvt>
2016-02-01 12:44:47 -0500

    ens to genename

--- src/ucsc.sh
+++ src/ucsc.sh
@@ -35,6 +35,28 @@ else
 fi
 }
 
+ucsc.ens2genename(){
+usage=" $FUNCNAME <file>
+"
+if [ $# -ne 1 ];then echo "$usage";return; fi
+
+        cat $1 | perl -e 'use strict;
+                my %eg=();
+                my $file=$ARGV[0];
+                open(F,$file) or die;
+                while(<F>){ my ($k,$v)=split /\s/,$_; $eg{$k}=$v; }
+                close(F);
+                while(<STDIN>){ chomp;
+                        if($_=~/(ENSG\d+)/){
+                                my $tmp=$eg{$1};
+                                if(defined $tmp){
+                                        $_=~s/ENSG\d+/$tmp/g;
+                                }
+                        }
+                        print $_,"\n";
+                }
+        ' $HMHOME/data/hg19/ensgToGenename.txt
+}
 ucsc.bg2bw(){
 usage="
 USAGE: $FUNCNAME <chrom.size> <bedGraph> [<bedGraph>]
