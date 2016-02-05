0b02227f8932616912549b5b5eb1ffaea51f5e45
hyunmin kim <oannes@hm.ucdenver.pvt>
2016-02-05 15:23:22 -0500

    fix divide by zeros

--- src/asp.sh
+++ src/asp.sh
@@ -46,7 +46,10 @@ asp.view tmp.gene tmp.read
 rm tmp.read tmp.gene
 }
 asp.spi(){
-usage=" $FUNCNAME <intron.bed6> <read.bed12> <strand> [<count>]
+usage=" 
+ FUNCT: Calculate SPI
+ OUTPU: bed6 a,b,c 2*a/(b+c)
+ USAGE: $FUNCNAME <intron.bed6> <read.bed12> <strand> [<count>]
 	<strand>: 0: null, 1:count on the same strand, 2: count on the opposite strand
 	<count> : 0: use as it is (default), 1: count as 1, 2: phred score
 	               (a)	
@@ -77,7 +80,9 @@ usage=" $FUNCNAME <intron.bed6> <read.bed12> <strand> [<count>]
 		| cut -f4,11 | stat.sum - | sort -k 1,1 > $tmpd/c
 	
 	join -a 1 -a 2 -e 0 -o 0,1.2,2.2 $tmpd/a $tmpd/b \
-	| join -a 1 -a 2 -e0 -o 0,1.2,1.3,2.2 - $tmpd/c | tr "@ " "\t"	
+	| join -a 1 -a 2 -e0 -o 0,1.2,1.3,2.2 - $tmpd/c \
+	| awk '{v=$3+$4; if(v==0){ v="inf";}else{ v=2*$2/v;}  print $1,$2,$3,$4,v;}' \
+	| tr "@ " "\t"	
 
 	rm -rf $tmpd;
 }
@@ -150,7 +155,7 @@ usage="
 
 	join -a 1 -a 2 -e 0 -o 0,1.2,2.2 $tmpd/a $tmpd/b \
 	| join -a 1 -a 2 -e 0 -o 0,1.2,1.3,2.2 - $tmpd/c \
-	| awk '{ print $1,$2","$3+$4,1-$2/($3+$4);}' \
+	| awk '{v=$3+4; if(v==0){ v="-inf";}else{ v=1 - $2/v;}  print $1,$2","$3+$4,v;}' \
 	| tr "@ " "\t"
 	
 	rm -rf $tmpd
