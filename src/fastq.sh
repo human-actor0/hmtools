33cb31cee6ca3bd3047d32521a4dcb4233d17ef7
hyunmin kim <oannes@hm.ucdenver.pvt>
2016-06-21 16:13:09 -0400

    cut3

--- src/fastq.sh
+++ src/fastq.sh
@@ -29,3 +29,50 @@ TTGATTA
 +
 IIIIIII" | fq.filter5 - TT
 }
+fq.cut3(){
+if [ $# -lt 2 ];then echo "
+$FUNCNAME <fastq> <3adapter> <mis>
+";fi
+	mycat $1 | fq.flat - | perl -ne 'BEGIN{ $T=0;$T1=0;} chomp; my $a="'$2'"; my $S=1; my $M='${3:-0}';
+		my ($id,$s,$o,$q)=split/\t/,$_;
+		#print $_," ",$a," ",length($s)," ",length($a),"\n";
+		$T++;
+		for(my $i=$S; $i<length($s)-length($a)+1+$M;$i++){
+			my $m=0;
+			for(my $j=0; $j < length($a); $j++){
+				if( $i+$j > length($s)-1){ $m+= length($a)-$j; last}
+				my $sj=substr($s,$i+$j,1);
+				my $aj=substr($a,$j,1);
+				if( $sj ne $aj){ $m++;}
+			}
+			if($m <= $M){
+				$T1++;
+				print $id,"\n";
+				print substr($s,0,$i),"\n";
+				print $o,"\n";
+				print substr($q,0,$i),"\n";
+				last;
+			}
+		}
+		END{ print {*STDERR} print "$T1/$T passed\n";}
+	'
+
+}
+
+fq.cut3.test(){
+echo \
+"@a
+CGGGCTTGAACACG
++
+IIIIIIIIIIIIII" > tmp.a
+cat tmp.a
+
+#fq.cut3 tmp.a GAAC 
+#fq.cut3 tmp.a GAACACG 
+#fq.cut3 tmp.a AGAACACG 1 
+fq.cut3 tmp.a GAACACGT 0 
+fq.cut3 tmp.a GAACACGT 1 
+rm tmp.a
+}
+
+
