b49b32225c156d22a948b861de3b93a3436ed855
hyunmin kim <oannes@hm.ucdenver.pvt>
2016-08-19 19:14:30 -0400

    fastq go

--- src/fastq.sh
+++ src/fastq.sh
@@ -1,68 +1,29 @@
 . $HMHOME/src/root.sh;
 
+#eval type mymktempd
 
-fq.flat(){
+fastq.flat(){
 	awk '{printf("%s",$0); if(NR%4==0){ printf("\n");}else{ printf("\t");}}' $1;
 }
-fq.filter5(){
+
+fastq.cut5p_into_head(){
 usage="
-USAGE: $FUNCNAME <fq> <barcode>
+FUNCTION: Cut 5p barcode and add it to the header (at the end of first non-space tocken)
+USAGE: $FUNCNAME <fastq> <len>
 "
-if [ $# -ne 2 ]; then echo "$usage"; return; fi
-	mycat $1 | fq.flat -  \
-	| perl -ne 'my $bc="'$2'";
-		chomp; my @a=split/\t/,$_;
-		if( $a[1]=~/^$bc/){
-			print join("\n",@a),"\n";
-		}
-	'
-}
-
-fq.filter5.test(){
-echo \
-"@a
-GATTACA
-+
-IIIIIII
-@a
-TTGATTA
-+
-IIIIIII" | fq.filter5 - TT
-}
-fq.cut3(){
-if [ $# -lt 2 ];then echo "
-$FUNCNAME <fastq> <3adapter> <mis>
-";fi
-	mycat $1 | fq.flat - \
-	| perl -e 'use strict; 
-	my $a="'$2'"; my $S=1; my $M='${3:-0}';
-	my $T=0; my $T1=0; 
-	while(<STDIN>){chomp; my ($id,$s,$o,$q)=split/\t/,$_;
-		$T++;
-		for(my $i=$S; $i<length($s)-length($a)+1+$M;$i++){
-			my $m=0;
-			for(my $j=0; $j < length($a); $j++){
-				if( $i+$j > length($s)-1){ $m+= length($a)-$j; last}
-				my $sj=substr($s,$i+$j,1);
-				my $aj=substr($a,$j,1);
-				if( $sj ne $aj){ $m++;}
-			}
-			if($m <= $M){
-				$T1++;
-				print $id,"\n";
-				print substr($s,0,$i),"\n";
-				print $o,"\n";
-				print substr($q,0,$i),"\n";
-				last;
-			}
-		}
-	}
-	print {*STDERR} $T1,"/",$T," passed\n";
-	'
+if [ $# -ne 2];then echo "$usage";return; fi
+	fastq.flat $1 \
+        | perl -ne 'chomp; my @a=split/\t/,$_; my $L="'$2'";
+                my @b=split /\s+/,$a[0];
+                $a[0] = $b[0].":".substr($a[1],0,$L);
+                $a[1] = substr($a[1],$L);
+                $a[3] = substr($a[1],$L);
+                print join("\n",@a),"\n";
+        '
 
 }
 
-fq.cut3_go(){
+fq.cut3p_go(){
 usage="$FUNCNAME <input> <3adaptor> [<num_mis_match>]";
 local cmd='
 package main
@@ -123,7 +84,7 @@ local tmp=`mymktempd`; echo "$cmd" > $tmp/script.go;
 cat $1 | go run $tmp/script.go
 
 }
-fq.cut3.test(){
+fq.cut3p_go.test(){
 echo \
 "@a
 CGGGCTTGAACACG
@@ -142,8 +103,7 @@ CGGGCTTGAACACG
 +
 IIIIIIIIIIIIII"  > tmp.a
 
-fq.cut3_go tmp.a GAAC  
-cat tmp.a | fq.cut3_go - GAAC  1
+fq.cut3p_go tmp.a GAAC  
 #fq.cut3 tmp.a GAAC 
 #fq.cut3 tmp.a GAACACG 
 #fq.cut3 tmp.a AGAACACG 1 
@@ -151,5 +111,72 @@ cat tmp.a | fq.cut3_go - GAAC  1
 #fq.cut3 tmp.a GGGGGGG 0 
 #rm tmp.a
 }
+
+
+
+fastq.cut5(){ usage="
+$FUNCNAME <fastq> <len> 
+"
+	if [ $# -ne 2 ]; then echo "$usage"; return; fi
+}
+
+fq.filter5(){
+usage="
+USAGE: $FUNCNAME <fq> <barcode>
+"
+if [ $# -ne 2 ]; then echo "$usage"; return; fi
+	mycat $1 | fq.flat -  \
+	| perl -ne 'my $bc="'$2'";
+		chomp; my @a=split/\t/,$_;
+		if( $a[1]=~/^$bc/){
+			print join("\n",@a),"\n";
+		}
+	'
+}
+
+fq.filter5.test(){
+echo \
+"@a
+GATTACA
++
+IIIIIII
+@a
+TTGATTA
++
+IIIIIII" | fq.filter5 - TT
+}
+fq.cut3(){
+if [ $# -lt 2 ];then echo "
+$FUNCNAME <fastq> <3adapter> <mis>
+";fi
+	mycat $1 | fq.flat - \
+	| perl -e 'use strict; 
+	my $a="'$2'"; my $S=1; my $M='${3:-0}';
+	my $T=0; my $T1=0; 
+	while(<STDIN>){chomp; my ($id,$s,$o,$q)=split/\t/,$_;
+		$T++;
+		for(my $i=$S; $i<length($s)-length($a)+1+$M;$i++){
+			my $m=0;
+			for(my $j=0; $j < length($a); $j++){
+				if( $i+$j > length($s)-1){ $m+= length($a)-$j; last}
+				my $sj=substr($s,$i+$j,1);
+				my $aj=substr($a,$j,1);
+				if( $sj ne $aj){ $m++;}
+			}
+			if($m <= $M){
+				$T1++;
+				print $id,"\n";
+				print substr($s,0,$i),"\n";
+				print $o,"\n";
+				print substr($q,0,$i),"\n";
+				last;
+			}
+		}
+	}
+	print {*STDERR} $T1,"/",$T," passed\n";
+	'
+
+}
+
 
 
